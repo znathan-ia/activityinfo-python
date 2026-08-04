ce49a69235cefc2e70e6cf175805a4c8d3089aa2
znathan-ia <76473774+znathan-ia@users.noreply.github.com>
2026-08-04 18:53:57 +0000

    Add month_field to model exports

--- activityinfo/models/__init__.py
+++ activityinfo/models/__init__.py
@@ -2,14 +2,16 @@ from .database import Database, DatabaseResource, DatabaseUser
 from .form import FormSchema, FormRecord
 from .field import (
     Field, FieldOption,
-    text_field, narrative_field, quantity_field, date_field,
+    text_field, narrative_field, quantity_field, date_field, month_field,
     single_select_field, multi_select_field, reference_field, geopoint_field,
+    calculated_field, section_field,
 )
 
 __all__ = [
     "Database", "DatabaseResource", "DatabaseUser",
     "FormSchema", "FormRecord",
     "Field", "FieldOption",
-    "text_field", "narrative_field", "quantity_field", "date_field",
+    "text_field", "narrative_field", "quantity_field", "date_field", "month_field",
     "single_select_field", "multi_select_field", "reference_field", "geopoint_field",
+    "calculated_field", "section_field",
 ]
