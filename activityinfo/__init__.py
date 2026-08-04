2c9d485720cc32a64e6de96d01784bc74b46589a
znathan-ia <76473774+znathan-ia@users.noreply.github.com>
2026-08-04 18:53:23 +0000

    Add month_field to imports and exports

--- activityinfo/__init__.py
+++ activityinfo/__init__.py
@@ -6,14 +6,16 @@ Client Python complet pour l'API ActivityInfo.
 
 from .client import ActivityInfoClient
 from .models.field import (
-    text_field, narrative_field, quantity_field, date_field,
+    text_field, narrative_field, quantity_field, date_field, month_field,
     single_select_field, multi_select_field, reference_field, geopoint_field,
+    calculated_field, section_field,
 )
 
 __all__ = [
     "ActivityInfoClient",
-    "text_field", "narrative_field", "quantity_field", "date_field",
+    "text_field", "narrative_field", "quantity_field", "date_field", "month_field",
     "single_select_field", "multi_select_field", "reference_field", "geopoint_field",
+    "calculated_field", "section_field",
 ]
 
 __version__ = "0.1.0"
