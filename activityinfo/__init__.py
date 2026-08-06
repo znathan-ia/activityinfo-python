07454cbed970763cc8aae187c2aa4e299e1bf775
znathan-ia <76473774+znathan-ia@users.noreply.github.com>
2026-08-06 10:24:16 +0000

    Update __init__.py

--- activityinfo/__init__.py
+++ activityinfo/__init__.py
@@ -8,14 +8,14 @@ from .client import ActivityInfoClient
 from .models.field import (
     text_field, narrative_field, quantity_field, date_field, month_field,
     single_select_field, multi_select_field, reference_field, geopoint_field,
-    calculated_field, section_field,
+    calculated_field, section_field, subform_field,
 )
 
 __all__ = [
     "ActivityInfoClient",
     "text_field", "narrative_field", "quantity_field", "date_field", "month_field",
     "single_select_field", "multi_select_field", "reference_field", "geopoint_field",
-    "calculated_field", "section_field",
+    "calculated_field", "section_field", "subform_field",
 ]
 
 __version__ = "0.1.0"
